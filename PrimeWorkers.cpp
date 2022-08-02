d8294a6e5214b8953a40f149e865a31e22013cb3
Cowsyoumake <110433426+Cowsyoumake@users.noreply.github.com>
2022-08-01 21:37:17 -0400

    Input is now split in two and finds primes in two std::future threads. The returned lists are sorted before getting merged into the final output.

--- PrimeWorkers.cpp
+++ PrimeWorkers.cpp
@@ -1,5 +1,11 @@
+#include <algorithm>
+#include <future>
+#include <list>
 #include <iostream>
 #include <vector>
+
+using InputType = std::vector< unsigned int >;
+using OutputType = std::list< unsigned int >;
 
 bool IsPrimeNumber( unsigned int number )
 {
@@ -15,16 +21,38 @@ bool IsPrimeNumber( unsigned int number )
     return isPrime;
 }
 
+OutputType FindPrimes( const InputType& numbers )
+{
+    OutputType primeList;
+
+    for ( auto it = numbers.begin(); it != numbers.end(); it++ )
+        if ( IsPrimeNumber( *it ) )
+            primeList.push_back( *it );
+
+    primeList.sort();
+
+    return primeList;
+}
+
 int main()
 {
-    std::vector< unsigned int > input;
+    InputType input;
 
-    for ( auto i = 0; i <= 100; i++ )
+    for ( auto i = 100; i >= 0; i-- )
         input.push_back( i );
 
-    for ( auto it = input.begin(); it != input.end(); it++)
-    {
-        if ( IsPrimeNumber( *it ) )
-            std::cout << *it << std::endl;
-    }
+    auto halfSize = input.size() / 2;
+    InputType lowerInputHalf( input.begin(), input.begin() + halfSize );
+    InputType upperInputHalf( input.begin() + halfSize, input.end() );
+    
+    std::future< OutputType > futureOne = std::async( FindPrimes, lowerInputHalf );
+    std::future< OutputType > futureTwo = std::async( FindPrimes, upperInputHalf );
+
+    auto output = futureOne.get();
+    output.merge( futureTwo.get() );
+
+    std::cout << "Output size is " << output.size() << std::endl;
+
+    for ( auto it = output.begin(); it != output.end(); it++ )
+        std::cout << *it << std::endl;
 }
